79d78acfeae9c4260996b29d4968d920390d5e72
mumumu3560 <puzmgamgam121@gmail.com>
2024-01-08 15:37:34 +0900

    ここではプロフィールを編集するのだが、プロフィールに張るリンク周りの機能を追加

--- lib/pages/profile_page/components/settings/profile_setting.dart
+++ lib/pages/profile_page/components/settings/profile_setting.dart
@@ -42,13 +42,20 @@ class _ProfileSettingsState extends State<ProfileSettings> {
   List<int> years = List.generate(130, (index) => DateTime.now().year - index); // 130年前~現在までの年度ギネスだと122らしい
 
   //リンクの入力コントローラー
-  TextEditingController _linkController = TextEditingController();
+  TextEditingController _linkController1 = TextEditingController();
+  TextEditingController _linkController2 = TextEditingController();
+  TextEditingController _linkController3 = TextEditingController();
+
 
   //説明文の入力コントローラー
     
   String explainText = "";
 
-  String linkText = "";
+  List<dynamic> linkText = [];
+
+  String linkText1 = "";
+  String linkText2 = "";
+  String linkText3 = "";
 
   int selectedYear = 2000; // 選択された年度
 
@@ -58,16 +65,31 @@ class _ProfileSettingsState extends State<ProfileSettings> {
   Future<void> getInfoFromSupabase() async{
     try{
 
+      print("エラー箇所はここの可能性");
       profileData = await supabase.from("profiles").select<List<Map<String, dynamic>>>().eq("id", myUserId);
+
+      print("ここが来ればおｋ");
 
       setState(() {
         userName = profileData[0]["username"];
         selectedYear = profileData[0]["age"];
         explainText = profileData[0]["explain"];
-        linkText = profileData[0]["Links"];
+        
+        if(profileData[0]["links"] != null){
+          linkText = profileData[0]["links"];
+          linkText1 = linkText[0];
+          linkText2 = linkText[1];
+          linkText3 = linkText[2];
+        }else{
+          linkText = [];
+        }
+
+        
 
         _explainController.text = explainText;
-        _linkController.text = linkText;
+        _linkController1.text = linkText1;
+        _linkController2.text = linkText2;
+        _linkController3.text = linkText3;
 
       });
       return ;
@@ -94,11 +116,13 @@ class _ProfileSettingsState extends State<ProfileSettings> {
 
     try{
 
+      linkText = [linkText1, linkText2, linkText3];
+
       await supabase.from("profiles").update({
         //"username": userName,
         "age": selectedYear,
         "explain": explainText,
-        "Links": linkText,
+        "links": linkText,
       }).eq("id", myUserId);
 
       return ;
@@ -135,58 +159,65 @@ class _ProfileSettingsState extends State<ProfileSettings> {
         title: const Text('プロフィール編集'),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Column(
+        child: Column(
+          
+          children: <Widget>[
+            Column(
 
+              children: [
 
-
-                children: [
-
-                  SizedBox(
+                Center(
+                  child: SizedBox(
                     width: SizeConfig.blockSizeHorizontal! * 90,
                     height: SizeConfig.blockSizeVertical! * 70,
                     child: IconSettings(profileImage: widget.profileImage,),
                   ),
+                ),
 
-                  
+                
 
 
 
-                  SizedBox(
-                    width: SizeConfig.blockSizeHorizontal! * 90,
-                    child: TextFormField(
-                      
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 3,
-                      maxLength: 100,
-                      controller: _explainController,
-                      onChanged: (value) {
-                        setState(() {
-                          explainText = value;
-                        });
-                      },
-                  
-                      decoration: const InputDecoration(
-                        labelText: 'プロフィール説明文',
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal!*5),
+                      alignment: Alignment.centerLeft,
+                      width: SizeConfig.blockSizeHorizontal! * 95,
+                      child: TextFormField(
+                        
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 3,
+                        maxLength: 100,
+                        controller: _explainController,
+                        onChanged: (value) {
+                          setState(() {
+                            explainText = value;
+                          });
+                        },
+                    
+                        decoration: const InputDecoration(
+                          labelText: 'プロフィール説明文',
+                        ),
+                    
                       ),
-                  
                     ),
-                  ),
+                  ],
+                ),
+                
+                Column(
                   
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+                  children: [
 
-                      Row(
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal!*5),
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-
+                    
                           Container(
-                            alignment: Alignment.centerRight,
-                            width: 200,
                             child: const Text("誕生年")
                           ),
                           const SizedBox(width: 10),
@@ -198,7 +229,7 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                               border: Border.all(color: Colors.green), // 枠線を追加
                               borderRadius: BorderRadius.circular(8), // 枠線の角を丸める
                             ),
-
+                    
                             child: DropdownButton<int>(
                               
                               value: selectedYear,
@@ -219,67 +250,127 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                           ),
                         ],
                       ),
+                    ),
 
-                      
-                      
-                      Row(
+                    SizedBox(height: SizeConfig.blockSizeVertical!*3,),
+
+                    
+                    
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal!*5),
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                            alignment: Alignment.centerRight,
-                            width: 200,
-                            height: 100,
-                            child: const Text("twitter(X)のリンク")
+                            child: const Text("twitter(X)などのリンク(3つまで)")
                           ),
                           const SizedBox(width: 10),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            width: 300,
-                            height: 50,
-
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.green), // 枠線を追加
-                              borderRadius: BorderRadius.circular(8), // 枠線の角を丸める
-                            ),
-                            
-                            child: TextFormField(
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 1,
-                              controller: _linkController,
-                              onChanged: (value) {
-                                setState(() {
-                                  linkText = value;
-                                });
-                              },
-
-                              decoration: InputDecoration(
-                                border: InputBorder.none
-                              ),
+                          Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                width: 300,
                           
-                            ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.green), // 枠線を追加
+                                  borderRadius: BorderRadius.circular(8), // 枠線の角を丸める
+                                ),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: 1,
+                                  controller: _linkController1,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      linkText1 = value;
+                                    });
+                                  },
+                                                  
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none
+                                  ),
+                                                        
+                                ),
+                              ),
+
+                              SizedBox(height: 10,),
+
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                width: 300,
+                          
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.green), // 枠線を追加
+                                  borderRadius: BorderRadius.circular(8), // 枠線の角を丸める
+                                ),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: 1,
+                                  controller: _linkController2,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      linkText2 = value;
+                                    });
+                                  },
+                                                  
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none
+                                  ),
+                                                        
+                                ),
+                              ),
+
+                              SizedBox(height: 10,),
+
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                width: 300,
+                          
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.green), // 枠線を追加
+                                  borderRadius: BorderRadius.circular(8), // 枠線の角を丸める
+                                ),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: 1,
+                                  controller: _linkController3,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      linkText3 = value;
+                                    });
+                                  },
+                                                  
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none
+                                  ),
+                                                        
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
+                    ),
 
-                    ],
-                  ),
+                  ],
+                ),
 
 
 
-                  SizedBox(height: SizeConfig.blockSizeVertical! * 5),
+                SizedBox(height: SizeConfig.blockSizeVertical! * 5),
 
-                  ElevatedButton(
-                    onPressed: () async{
-                      await sendInfoToSupabase();
-                      Navigator.pop(context);
-                    },
-                    child: Text("更新"),
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ),
+                ElevatedButton(
+                  onPressed: () async{
+                    await sendInfoToSupabase();
+                    Navigator.pop(context);
+                  },
+                  child: Text("更新"),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
 
-            ],
-          ),
+          ],
         ),
       ),
     );
