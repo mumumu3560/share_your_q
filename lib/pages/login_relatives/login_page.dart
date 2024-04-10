08eb4d9352d6863b17b3155630383224e5297292
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-11 03:55:56 +0900

    Googleでのサインインの追加

--- lib/pages/login_relatives/login_page.dart
+++ lib/pages/login_relatives/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:share_your_q/env/env.dart';
 import 'package:share_your_q/pages/home_page/home_page_web.dart';
 import 'package:share_your_q/utils/various.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -49,6 +51,65 @@ class LoginPageState extends State<LoginPage> {
     }
   }
 
+
+
+
+  Future<void> _googleSignIn() async {
+
+    /// TODO: update the Web client ID with your own.
+    final webClientId = Env.g3;
+
+    /// TODO: update the iOS client ID with your own.
+    //const iosClientId = 'my-ios.apps.googleusercontent.com';
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      //clientId: iosClientId,
+      serverClientId: webClientId,
+    );
+
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null) {
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    await supabase.auth.signInWithIdToken(
+      //provider: OAuthProvider.google,
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+
+
+
+
+  void _setupAuthListener() {
+    supabase.auth.onAuthStateChange.listen((data) {
+      final event = data.event;
+      if (event == AuthChangeEvent.signedIn) {
+
+        Navigator.of(context).pushReplacement(HomePage.route());
+
+      }
+    });
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _setupAuthListener();
+  }
+
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -87,6 +148,38 @@ class LoginPageState extends State<LoginPage> {
             onPressed: _isLoading ? null : _signIn,
             child: const Text('ログイン'),
           ),
+
+          formSpacer,
+
+          ElevatedButton(
+              //背景色を変えたい
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.transparent, elevation: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: [
+                  Image.asset("assets/images/google-symbol.png", width: 24, height: 24),
+
+                  SizedBox(width: SizeConfig.blockSizeHorizontal! * 5,),
+                  const Text('Login with Google'),
+                ],
+              ),
+
+              onPressed: _isLoading ? null : () async {
+                try {
+                  await _googleSignIn();
+                } on PostgrestException catch (e) {
+                  if(context.mounted){
+                    context.showErrorSnackBar(message: e.message);
+                  }
+                } catch (e) {
+                  if(context.mounted){
+                    context.showErrorSnackBar(message: unexpectedErrorMessage);
+                  }
+                }
+              },
+            ),
 
         ],
       ),
