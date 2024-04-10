7e5f9fb4194ecd63abfe3100cbb906a56d2a4c3e
mumumu3560 <puzmgamgam121@gmail.com>
2024-04-11 03:56:29 +0900

    Googleでのサインインの追加とメール周りの追加

--- lib/pages/login_relatives/register_page.dart
+++ lib/pages/login_relatives/register_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:share_your_q/env/env.dart';
 import 'package:share_your_q/utils/various.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:share_your_q/pages/home_page/home_page_web.dart';
@@ -6,6 +9,7 @@ import 'package:share_your_q/pages/login_relatives/login_page.dart';
 import 'package:email_validator/email_validator.dart';
 import 'dart:async';
 
+import 'package:google_sign_in/google_sign_in.dart';
 
 //https://zenn.dev/dshukertjr/books/flutter-supabase-chat/viewer/page1
 
@@ -38,19 +42,38 @@ class _RegisterPageState extends State<RegisterPage> {
   late final StreamSubscription<AuthState> _authSubscription;
   final locale = WidgetsBinding.instance.platformDispatcher.locale;
 
-   @override
+  //TODO Google Auth
+  @override
   void initState() {
+
+
     super.initState();
 
     bool haveNavigated = false;
     //ここで認証のリッスンを行うsessionがあるならhomepageに移動
-    _authSubscription =
-        supabase.auth.onAuthStateChange.listen((data) {
+    _authSubscription = supabase.auth.onAuthStateChange.listen((data) {
+
       final session = data.session;
       if (session != null && !haveNavigated) {
         haveNavigated = true;
-        Navigator.of(context)
-            .pushReplacement(HomePage.route());
+        Navigator.of(context).pushReplacement(HomePage.route());
+      }
+
+    });
+
+    _setupAuthListener();
+
+
+
+  }
+
+  void _setupAuthListener() {
+    supabase.auth.onAuthStateChange.listen((data) {
+      final event = data.event;
+      if (event == AuthChangeEvent.signedIn) {
+
+        Navigator.of(context).pushReplacement(HomePage.route());
+
       }
     });
   }
@@ -62,7 +85,45 @@ class _RegisterPageState extends State<RegisterPage> {
     //認証の監視をキャンセル
     _authSubscription.cancel();
   }
+
+
+
+  Future<void> _googleSignIn() async {
+
+    /// TODO: update the Web client ID with your own.
+    final webClientId = Env.g3;
+
+    /// TODO: update the iOS client ID with your own.
+    //const iosClientId = 'my-ios.apps.googleusercontent.com';
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      //clientId: iosClientId,
+      serverClientId: webClientId,
+    );
+
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null) {
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    await supabase.auth.signInWithIdToken(
+      //provider: OAuthProvider.google,
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+
+
   
+
 
   Future<void> _signUp() async {
     //これでformのvalidationを確認
@@ -78,12 +139,12 @@ class _RegisterPageState extends State<RegisterPage> {
 
     try {
 
-      showLoadingDialog(context, "登録中...");
+      showLoadingDialog(context, "送信中...");
       //https://supabase.com/docs/guides/getting-started/tutorials/with-flutter?platform=android
       await supabase.auth.signUp(
           email: email, 
           password: password, 
-          data: {'username': username , 'locale': locale.toString()/* */},
+          data: {'name': username , 'locale': locale.toString()/* */},
           emailRedirectTo: 'io.supabase.shareimage://login',
         );
       
@@ -93,13 +154,45 @@ class _RegisterPageState extends State<RegisterPage> {
         
 
       if (!mounted) return;
-      context.showSuccessSnackBar(message: 'メールを送りました。認証を行ってください');
+
+      //TODO メールを送信した後の処理
+      //Dialogを出す。
+
+      showDialog(
+        context: context, 
+        builder: (BuildContext context) {
+          return AlertDialog(
+              
+            title: const Text("メールを送信しました。"),
+            content: const Text("メールを確認して認証を行ってください。"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // ダイアログを閉じる
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+
+
+      //
+      //
+      //context.showSuccessSnackBar(message: 'メールを送りました。認証を行ってください');
       //Navigator.of(context).pushAndRemoveUntil(HomePage.route(), (route) => false);
 
     } on AuthException catch (error) {
+      if(mounted){
+        Navigator.of(context).pop();
+      }
       if (!mounted) return;
       context.showErrorSnackBar(message: error.message);
     } catch (error) {
+      if(mounted){
+        Navigator.of(context).pop();
+      }
       if (!mounted) return;
       context.showErrorSnackBar(message: unexpectedErrorMessage);
     }
@@ -108,6 +201,7 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('登録'),
@@ -172,9 +266,9 @@ class _RegisterPageState extends State<RegisterPage> {
                 if (val == null || val.isEmpty) {
                   return '必須';
                 }
-                final isValid = RegExp(r'^[A-Za-z0-9_]{3,24}$').hasMatch(val);
+                final isValid = RegExp(r'^.{3,24}$').hasMatch(val);
                 if (!isValid) {
-                  return '3~24文字のアルファベットか文字で入力してください';
+                  return '3~24文字で入力してください';
                 }
                 return null;
               },
@@ -188,6 +282,38 @@ class _RegisterPageState extends State<RegisterPage> {
             ),
 
             formSpacer,
+
+            ElevatedButton(
+              //背景色を変えたい
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.transparent, elevation: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: [
+                  Image.asset("assets/images/google-symbol.png", width: 24, height: 24),
+
+                  SizedBox(width: SizeConfig.blockSizeHorizontal! * 5,),
+                  const Text('Login with Google'),
+                ],
+              ),
+
+              onPressed: _isLoading ? null : () async {
+                try {
+                  await _googleSignIn();
+                } on PostgrestException catch (e) {
+                  if(context.mounted){
+                    context.showErrorSnackBar(message: e.message);
+                  }
+                } catch (e) {
+                  if(context.mounted){
+                    context.showErrorSnackBar(message: unexpectedErrorMessage);
+                  }
+                }
+              },
+            ),
+
+            
             TextButton(
               onPressed: () {
                 Navigator.of(context).push(LoginPage.route());
